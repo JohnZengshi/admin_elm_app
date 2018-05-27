492fe4d586a2630fb97656d89fd1c0ff6556c2fb
JOHNPC\JohnZeng <1351009316@qq.com>
2018-05-27 16:11:26 +0800

    增加router-view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
     <div id="app" class="fillcontain">
-        <h1>主页</h1>
-        <!-- <router-view></router-view> -->
+        <router-view></router-view>
     </div>
 </template>
 <script>
