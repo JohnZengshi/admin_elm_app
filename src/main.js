c5b674c65c1519a7ffa7453a753256d67c56c022
JOHNPC\JohnZeng <1351009316@qq.com>
2018-05-27 16:15:14 +0800

    增加elem插件

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import App from './App';
+import router from './router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
 // Vue去掉警告 You are running Vue in development mode
 Vue.config.productionTip = false;
-
+Vue.use(ElementUI);
 new Vue({
     el: "#app",
     template: "<App/>",
+    router,
     components: { App }
 })
