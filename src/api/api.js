a2430142223b727d447f9f3b925ad85fd33fad1d
JOHNPC\JohnZeng <1351009316@qq.com>
2018-05-27 21:49:01 +0800

    新增管理员注册接口

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,14 @@ import request from '../common/js/request';
 const testApi = "http://192.168.1.101:8081";
 
 const adminLogin = async (data) => {
-    const res = await request('POST', data, testApi + '/admin/login')
+    const res = await request('POST', data, testApi + '/admin/login');
+    return res;
+}
+const adminRegister = async (data) => {
+    const res = await request('POST', data, testApi + '/admin/register');
     return res;
 }
 export default {
     adminLogin,
+    adminRegister
 }
