3d68c55e64d2ec892754a11fe313fbf4315cae35
JOHNPC\JohnZeng <1351009316@qq.com>
2018-05-27 21:50:59 +0800

    测试组件

---
+++ src/page/view/path1.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1 v-loading="loading">path1</h1>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                loading: true
+            }
+        }
+    }
+
+</script>
+<style lang="less">
+
+
+</style>
