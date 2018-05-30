57dbbae7e20fe5fb60843771683bf85f6c2ced47
JOHNPC\JohnZeng <1351009316@qq.com>
2018-05-30 22:28:31 +0800

    增加获取api请求总数

--- src/api/api.js
+++ src/api/api.js
@@ -1,20 +1,27 @@
 import request from '../common/js/request';
 const testApi = "http://192.168.1.101:8081";
-
+// 用户登录
 const adminLogin = async (data) => {
     const res = await request('POST', data, testApi + '/admin/login');
     return res;
 }
+// 用户注册
 const adminRegister = async (data) => {
     const res = await request('POST', data, testApi + '/admin/register');
     return res;
 }
+// 检测用户是否已经登录
 const checkAdminLogin = async (data) => {
     const res = await request('GET', data, testApi + '/admin/info?t=' + new Date().getTime().toString());
+    return res;
+}
+const apiCount = async (data) => {
+    const res = await request('GET', data, testApi + '/statis/api/count');
     return res;
 }
 export default {
     adminLogin,
     adminRegister,
-    checkAdminLogin
+    checkAdminLogin,
+    apiCount
 }
